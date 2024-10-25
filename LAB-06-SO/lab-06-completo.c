3c1629827e51e71d133b64c9b7ec2470ee8f7e1e
MihaelRBX <145815987+MihaelRBX@users.noreply.github.com>
2024-10-24 23:34:58 -0300

    Add files via upload

---
+++ LAB-06-SO/lab-06-completo.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/sysctl.h>  /*utilizando macOS tive que usar esse cabecalho e a função sysctl() para acessar informações
+do sistema, como o tamanho total de memória RAM (HW_MEMSIZE).*/
+
+struct reg {
+    int conteudo;
+    struct reg *prox;
+};
+
+typedef struct reg celula;
+
+
+void imprime_lista(celula *le) {
+    celula *p = le;
+    printf("Elementos da lista: ");
+    while (p != NULL) {
+        printf("%d ", p->conteudo);
+        p = p->prox;
+    }
+    printf("\n");
+}
+
+void remove_elemento(celula **le, int valor) {
+    celula *atual = *le, *anterior = NULL;
+
+    while (atual != NULL && atual->conteudo != valor) {
+        anterior = atual;
+        atual = atual->prox;
+    }
+
+    if (atual == NULL) return;
+
+    if (anterior == NULL) {
+        *le = atual->prox;
+    } else {
+        anterior->prox = atual->prox;
+    }
+
+    free(atual);
+}
+
+
+void calcula_tamanho_celula() {
+    printf("Tamanho de uma célula: %zu bytes\n", sizeof(celula));
+}
+
+//obtendo a quantidade de memória total no macOS
+void calcula_memoria_total() {
+    int mib[2];
+    int64_t memoria_total;
+    size_t len = sizeof(memoria_total);
+
+    mib[0] = CTL_HW;
+    mib[1] = HW_MEMSIZE;
+
+    sysctl(mib, 2, &memoria_total, &len, NULL, 0);
+    printf("Memória total do sistema: %lld bytes\n", memoria_total);
+}
+
+void libera_lista(celula *le) {
+    celula *atual = le;
+    celula *proximo;
+
+    while (atual != NULL) {
+        proximo = atual->prox;
+        free(atual);
+        atual = proximo;
+    }
+}
+
+
+int main() {
+    
+    celula *primeira = (celula *)malloc(sizeof(celula));
+    celula *segunda = (celula *)malloc(sizeof(celula));
+    celula *terceira = (celula *)malloc(sizeof(celula));
+
+    primeira->conteudo = 1;
+    primeira->prox = segunda;
+    
+    segunda->conteudo = 2;
+    segunda->prox = terceira;
+    
+    terceira->conteudo = 3;
+    terceira->prox = NULL; 
+
+    imprime_lista(primeira);
+
+    calcula_tamanho_celula();
+
+    calcula_memoria_total();
+
+    remove_elemento(&primeira, 2);
+    printf("Após remover o elemento com valor 2:\n");
+    imprime_lista(primeira);
+
+    libera_lista(primeira);
+
+    return 0;
+}
